e9f73100b9387e6a4dc08e011e137d051d9b6b5e
Shirley Nakalema <shirley.nakalema@contango.no>
2021-07-09 02:01:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-function compute() {
-    {
+function compute()
+{
+    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
+    var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-    }
 
-        function updateRate() 
-    {
+    <!-- Validando entrada amount maior que 0-->    
+    if (principal <= 0){
+        alert("Amount must be higher than 0");
+        return false;
+    }
+    
+    <!-- Calculo para resultado final-->
+    document.getElementById("result").innerHTML= "\<br>If you deposit \<mark> " +  principal  + ", \</mark> \<br>at an interest rate of  \<mark>" + 
+     rate  + "%. \</mark> \<br> You will receive an amount of \<mark>" +  interest  + ", \</mark> \<br>in the year \<mark>"+  year  +" \</mark> \<br>"
+
+    
+}
+
+function updateRate()
+{
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-    }
-
-if (int === "si") {
-    var sip = (principal * rate * year) / 100
-    var ta = principal + sip
-    document.getElementById("i").innerHTML = sip;
-    document.getElementById("a").innerHTML = ta;
-  } else {
-    var cta = principal * (Math.pow((1 + r / 100), t))
-    var cmp = cta - principal
-    document.getElementById("i").innerHTML = cmp;
-    document.getElementById("a").innerHTML = cta;
-  }
 }
+        
