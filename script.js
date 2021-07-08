ecb7fc5954d6e080adf0723b57a05374a9be7ca6
Shirley Nakalema <shirley.nakalema@contango.no>
2021-07-08 22:20:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-function compute()
-{
+function compute() {
+    {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-}
+    }
 
         function updateRate() 
-{
+    {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-}
+    }
 
-{
-    var result = document.getElementById("result").innerHTML="
-    If you deposit "+principal+",\<br\>
-at an interest rate of "+rate+"%\<br\>
-You will receive an amount of "+amount+",\<br\>
-in the year "+year+"\<br\>"
+if (int === "si") {
+    var sip = (p * r * t) / 100
+    var ta = p + sip
+    document.getElementById("i").innerHTML = sip;
+    document.getElementById("a").innerHTML = ta;
+  } else {
+    var cta = p * (Math.pow((1 + r / 100), t))
+    var cmp = cta - p
+    document.getElementById("i").innerHTML = cmp;
+    document.getElementById("a").innerHTML = cta;
+  }
 }
